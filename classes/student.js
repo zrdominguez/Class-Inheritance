e1e8494b6ee36bd48af777635c02184bb52b068a
zrdominguez <zechariahd@gmail.com>
2024-03-06 19:31:05 -0600

    finished student tests

--- classes/student.js
+++ classes/student.js
@@ -1,6 +1,21 @@
 const Person = require('./person');
 
-// Your code here 
+class Student extends Person {
+  constructor(firstName, lastName, major, GPA){
+    super(firstName, lastName)
+    this.major = major;
+    this.GPA = GPA;
+  }
+  static compareGPA(student1, student2){
+    if(student1.GPA == student2.GPA) return  "Both students have the same GPA"
+    let smartStudent = [student1, student2].reduce((acc, el)=>{
+      if(acc.GPA < el.GPA) acc = el;
+      return acc
+    })
+    return `${smartStudent.firstName} ${smartStudent.lastName} has the higher GPA.`
+  }
+
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
